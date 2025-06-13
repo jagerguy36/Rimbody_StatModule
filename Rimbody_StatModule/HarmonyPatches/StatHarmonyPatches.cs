1c1f6cb9c1ac7bf8a840a1e980f51d4535fd5ea1
jagerguy36 <141026751+jagerguy36@users.noreply.github.com>
2025-06-13 12:20:26 +0900

    1.1.1 Minor fix, requirement warning and performance update

--- Rimbody_StatModule/HarmonyPatches/StatHarmonyPatches.cs
+++ Rimbody_StatModule/HarmonyPatches/StatHarmonyPatches.cs
@@ -28,13 +28,11 @@ namespace Rimbody_StatModule
     [HarmonyPatch(typeof(Need_Food), "get_MalnutritionSeverityPerInterval")]
     static class MalnutritionPatch
     {
-        static bool Prefix(ref float __result, Need_Food __instance)
+        static bool Prefix(ref float __result, Need_Food __instance, Pawn ___pawn)
         {
             if (__instance.Starving == true)
             {
-                var pawnField = typeof(Need).GetField("pawn", BindingFlags.NonPublic | BindingFlags.Instance);
-                var pawn = (Pawn)pawnField.GetValue(__instance);
-                var compPhysique = pawn.compPhysique();
+                var compPhysique = ___pawn.compPhysique();
                 if (compPhysique != null)
                 {
                     __result = 0.0011325f * (1.2f - (0.7f * Mathf.Pow(compPhysique.BodyFat / 50, 2)));
--- Rimbody_StatModule/Rimbody_StatModule.cs
+++ Rimbody_StatModule/Rimbody_StatModule.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Verse;
 
 namespace Rimbody_StatModule
 {
-    internal class Rimbody_StatModule
+    public class Rimbody_StatModule : Mod
     {
+        public Rimbody_StatModule(ModContentPack content) : base(content)
+        {
+            if (!ModsConfig.IsActive("Maux36.Rimbody"))
+            {
+                Log.Error("Rimbody_stats module could not find its required dependency: Rimbody. This is a critical component, and your game will not work without it.");
+            }
+            if (!ModsConfig.IsActive("zetrith.prepatcher"))
+            {
+                Log.Error("Rimbody_stats module could not find Prepatcher. This means your Rimbody is not relying on prepatcher. This is a critical component, and your game will not work without it. If you are using Rimbody version 1.0.x, you should downgrade your stats module to 1.0.x by going to the mod's github page.");
+            }
+        }
     }
 }
